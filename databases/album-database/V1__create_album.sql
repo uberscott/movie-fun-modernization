593f4c3d7ea6f0c12d9eece43c972b4dc89a2b9d
Scott Williams <scott@mightydevco.com>
2018-08-29 16:58:46 -0600

    blah

--- databases/album-database/V1__create_album.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE album
-(
-	  id     BIGINT(20) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-	  artist VARCHAR(255),
-	  rating INT(11)                NOT NULL,
-	  title  VARCHAR(255),
-	  year   INT(11)                NOT NULL
-);
--- databases/album-database/V2__create_album_scheduler.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE album_scheduler_task (
-	  started_at TIMESTAMP NULL DEFAULT NULL
-);
-
-INSERT INTO album_scheduler_task (started_at) VALUES (NULL);
